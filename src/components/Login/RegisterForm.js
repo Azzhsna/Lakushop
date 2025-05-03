034f34c758d58b45b5a729681d061310f09440f7
Azzah <azzhhsna08@gmail.com>
2025-05-03 14:03:50 +0700

    update

--- src/components/Login/RegisterForm.js
+++ src/components/Login/RegisterForm.js
@@ -31,69 +31,66 @@ function RegisterForm({ onSwitch }) {
 
     if (Object.keys(newErrors).length === 0) {
       console.log("Data pendaftaran:", form);
-      // Submit form ke backend di sini
     }
   };
 
   return (
-    <div className="login-container">
-      <div className="Register-form">
-        <h2>Daftar / Register</h2>
+    <div className="Register-form">
+      <h2>Daftar / Register</h2>
 
-        <form onSubmit={handleSubmit}>
-          <label>Nama Depan</label>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="Masukkan Nama Depan"
-            value={form.firstName}
-            onChange={handleChange}
-          />{" "}
-          {errors.firstName && <p className="error">{errors.firstName}</p>}
-          <label>Nama Belakang</label>
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Masukkan Nama Belakang"
-            value={form.lastName}
-            onChange={handleChange}
-          />{" "}
-          {errors.lastName && <p className="error">{errors.lastName}</p>}
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Masukkan Email"
-            value={form.email}
-            onChange={handleChange}
-          />{" "}
-          {errors.email && <p className="error">{errors.email}</p>}
-          <label>Nomor Handphone</label>
-          <input
-            type="text"
-            name="phone"
-            placeholder="Masukkan Nomor Handphone"
-            value={form.phone}
-            onChange={handleChange}
-          />{" "}
-          {errors.phone && <p className="error">{errors.phone}</p>}
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Masukkan Password"
-            value={form.password}
-            onChange={handleChange}
-          />{" "}
-          {errors.password && <p className="error">{errors.password}</p>}
-          <button className="login-btn" onClick={(e) => handleSubmit(e)}>
-            Daftar
-          </button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <label>Nama Depan</label>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Masukkan Nama Depan"
+          value={form.firstName}
+          onChange={handleChange}
+        />{" "}
+        {errors.firstName && <p className="error">{errors.firstName}</p>}
+        <label>Nama Belakang</label>
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Masukkan Nama Belakang"
+          value={form.lastName}
+          onChange={handleChange}
+        />{" "}
+        {errors.lastName && <p className="error">{errors.lastName}</p>}
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Masukkan Email"
+          value={form.email}
+          onChange={handleChange}
+        />{" "}
+        {errors.email && <p className="error">{errors.email}</p>}
+        <label>Nomor Handphone</label>
+        <input
+          type="text"
+          name="phone"
+          placeholder="Masukkan Nomor Handphone"
+          value={form.phone}
+          onChange={handleChange}
+        />{" "}
+        {errors.phone && <p className="error">{errors.phone}</p>}
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Masukkan Password"
+          value={form.password}
+          onChange={handleChange}
+        />{" "}
+        {errors.password && <p className="error">{errors.password}</p>}
+        <button className="login-btn" onClick={(e) => handleSubmit(e)}>
+          Daftar
+        </button>
+      </form>
 
-        <div className="register-text" onClick={onSwitch}>
-          <a> Sudah punya akun?</a>
-        </div>
+      <div className="register-text" onClick={onSwitch}>
+        <a> Sudah punya akun?</a>
       </div>
     </div>
   );
